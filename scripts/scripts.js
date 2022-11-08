452e53a04e15dd42db71cdf40d34869381ba1dd0
Evgeny <nk-nazarovEA@yandex.ru>
2022-11-09 00:55:28 +0300

    1 коммит

---
+++ scripts/scripts.js
@@ -0,0 +1,50 @@
+const popup = document.querySelector('.popup');
+const popupOpen = document.querySelector('.profile__profile-info_edit-button');
+const popupClose = document.querySelector('.popup__close');
+const nameValue = document.querySelector('.profile__profile-info_name');
+const jobValue = document.querySelector('.profile__profile-info_profession');
+const formElement = document.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.popup__input_type_name');
+const jobInput = formElement.querySelector('.popup__input_type_job');
+// открываем попап
+popupOpen.addEventListener('click', function (event) {
+event.preventDefault();
+popup.classList.add('popup_opened');
+nameInput.value = nameValue.textContent;
+jobInput.value = jobValue.textContent;
+});
+// закрываем попап
+popupClose.addEventListener('click', () => {
+popup.classList.remove('popup_opened');
+});
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler(evt) {
+evt.preventDefault();
+nameValue.textContent = nameInput.value;
+jobValue.textContent = jobInput.value;
+popup.classList.remove('popup_opened');
+}
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
